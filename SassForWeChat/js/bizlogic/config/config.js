db1edd61bed8619d57ca05daac135d5bbb9ac094
housc <1162138903@qq.com>
2017-01-09 13:46:13 +0800

    ConfigFile Change

--- SassForWeChat/js/bizlogic/config/config.js
+++ SassForWeChat/js/bizlogic/config/config.js
@@ -25,13 +25,13 @@ define(function(require, exports, module) {
 	 */
 	(function() {
 		//正式地址
-		var serverUrl_formal = 'http://218.4.136.120:8086/MobileServiceAPI';
+		var serverUrl_formal = 'https://218.4.136.120:8086/MobileServiceAPI';
 		var udateFileUrl_formal = '';
 		//测试地址
 		var serverUrl_test = '';
 		var udateFileUrl_test = '';
 		//mock地址
-		var serverUrl_mock = 'http://218.4.136.118:8086/mockjs/95/';
+		var serverUrl_mock = 'https://218.4.136.118:8086/mockjs/95/';
 		if(exports.isFormal === true) {
 			exports.serverUrl = serverUrl_formal;
 			exports.spdateFileUrl = udateFileUrl_formal;
